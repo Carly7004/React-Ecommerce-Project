5058c1f7670d01edc9e54c5be837b39b35386df7
Carly7004 <91916651+Carly7004@users.noreply.github.com>
2023-09-27 10:33:06 +0200

    finished setting pagination on the product page

--- src/components/PaginationContainer.jsx
+++ src/components/PaginationContainer.jsx
@@ -1,4 +1,64 @@
+import { useLoaderData, useNavigate, useLocation } from 'react-router-dom';
+
 const PaginationContainer = () => {
-  return <div>PaginationContainer</div>;
+  const { metaData } = useLoaderData();
+  const { pageCount, page } = metaData.pagination;
+
+  const pages = Array.from({ length: pageCount }, (_, index) => {
+    return index + 1;
+  });
+
+  const { search, pathname } = useLocation();
+  const navigate = useNavigate();
+
+  const handlePageChange = (pageNumber) => {
+    const searchParams = new URLSearchParams(search);
+    searchParams.set('page', pageNumber);
+    navigate(`${pathname}?${searchParams.toString()}`);
+  };
+
+  if (pageCount < 2) return null;
+
+  return (
+    <div className='mt-16 flex justify-center'>
+      <div className='join'>
+        <button
+          className='btn btn-xs sm:btn-md join-item'
+          onClick={() => {
+            let prevPage = page - 1;
+            if (prevPage < 1) prevPage = pageCount;
+            handlePageChange(prevPage);
+          }}
+        >
+          Prev
+        </button>
+      </div>
+      {pages.map((pageNumber) => {
+        return (
+          <button
+            key={pageNumber}
+            onClick={() => handlePageChange(pageNumber)}
+            className={`btn btn-xs sm:btn-md border-none join-item ${
+              pageNumber === page ? 'bg-base-300 border-base-300' : ''
+            }`}
+          >
+            {pageNumber}
+          </button>
+        );
+      })}
+      <div className='join'>
+        <button
+          className='btn btn-xs sm:btn-md join-item'
+          onClick={() => {
+            let nextPage = page + 1;
+            if (nextPage > pageCount) nextPage = pageCount;
+            handlePageChange(nextPage);
+          }}
+        >
+          next
+        </button>
+      </div>
+    </div>
+  );
 };
 export default PaginationContainer;
